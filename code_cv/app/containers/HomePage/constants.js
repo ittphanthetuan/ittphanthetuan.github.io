cb2e84f630457c552c6327cb0d03ccab71748b95
ittphanthetuan <ittphanthetuan@gmail.com>
2021-04-20 21:54:45 +0700

    update

--- code_cv/app/containers/HomePage/constants.js
+++
@@ -1,2 +0,0 @@
-export const CALL_DATA_CV = 'boilerplate/Home/CALL_DATA_CV';
-export const SET_DATA_CV = 'boilerplate/Home/SET_DATA_CV';
--- code_cv/app/mockup/education.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: 'deducation',
-  listInfo: [
-    {
-      title: '2010 - 2013',
-      list: ['Software Engineer, in Industrial University of Ho Chi Minh City'],
-    },
-  ],
-};
